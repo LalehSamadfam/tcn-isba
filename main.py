a106f0818e818a758dc0b10e9a9f83f4ba81a29e
LalehSamadfam <laleh.samadfam@gmail.com>
2020-09-27 16:03:07 +0200

    main fine refactored

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from batch_gen import BatchGenerator
 import os
 import argparse
 import random
+import numpy as np
 
+
+# init
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 seed = 1538574472
@@ -17,40 +20,39 @@ torch.backends.cudnn.deterministic = True
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--action', default='train')
-parser.add_argument('--dataset', default="gtea")
-parser.add_argument('--split', default='1')
+parser.add_argument('--dataset', default="breakfast")
+parser.add_argument('--split', default='2')
 
 args = parser.parse_args()
 
-num_stages = 4
+num_stages = 1 #TODO change it to 2
 num_layers = 10
 num_f_maps = 64
 features_dim = 2048
 bz = 1
 lr = 0.0005
-num_epochs = 50
+num_epochs = 1 #TODO change it to 50
 
 # use the full temporal resolution @ 15fps
 sample_rate = 1
-# sample input features @ 15fps instead of 30 fps
-# for 50salads, and up-sample the output to 30 fps
-if args.dataset == "50salads":
-    sample_rate = 2
 
-vid_list_file = "./data/"+args.dataset+"/splits/train.split"+args.split+".bundle"
-vid_list_file_tst = "./data/"+args.dataset+"/splits/test.split"+args.split+".bundle"
-features_path = "./data/"+args.dataset+"/features/"
-gt_path = "./data/"+args.dataset+"/groundTruth/"
+vid_list_file = "./data/" + args.dataset + "/splits/train.split" + args.split + ".bundle"
+vid_list_file_tst = "./data/" + args.dataset + "/splits/test.split" + args.split + ".bundle"
+features_path = "./data/" + args.dataset + "/features/"
+#gt_path = "./data/" + args.dataset + "/groundTruth/"
+segmentation_path = "./data/" + args.dataset + "/segmentation/"
 
-mapping_file = "./data/"+args.dataset+"/mapping.txt"
+mapping_file = "./data/" + args.dataset + "/mapping.txt"
 
-model_dir = "./models/"+args.dataset+"/split_"+args.split
-results_dir = "./results/"+args.dataset+"/split_"+args.split
- 
+model_dir = "./models/" + args.dataset + "/split_" + args.split
+results_dir = "./results/" + args.dataset + "/split_" + args.split
+temp_results_dir = "./temp_results/" + args.dataset + "/split_" + args.split
+
 if not os.path.exists(model_dir):
     os.makedirs(model_dir)
 if not os.path.exists(results_dir):
     os.makedirs(results_dir)
+
 
 file_ptr = open(mapping_file, 'r')
 actions = file_ptr.read().split('\n')[:-1]
@@ -61,11 +63,26 @@ for a in actions:
 
 num_classes = len(actions_dict)
 
-trainer = Trainer(num_stages, num_layers, num_f_maps, features_dim, num_classes)
+
+# train
+
+loss = float('inf')
+no_enhancement = 0
+
 if args.action == "train":
-    batch_gen = BatchGenerator(num_classes, actions_dict, gt_path, features_path, sample_rate)
+    batch_gen = BatchGenerator(num_classes, actions_dict, segmentation_path, features_path, sample_rate)
     batch_gen.read_data(vid_list_file)
-    trainer.train(model_dir, batch_gen, num_epochs=num_epochs, batch_size=bz, learning_rate=lr, device=device)
+    trainer = Trainer(num_stages, num_layers, num_f_maps, features_dim, num_classes, batch_gen.class_weights)
+    while no_enhancement < 3:
+        trainer.train(model_dir, batch_gen, num_epochs=num_epochs, batch_size=bz, learning_rate=lr, device=device)
+        predictions, prediction_probs = trainer.predict(model_dir, results_dir, features_path, vid_list_file,
+                                      num_epochs, actions_dict, device, sample_rate)
+        new_loss = batch_gen.loss(prediction_probs)
+        if new_loss > loss:
+            no_enhancement += 1
+        loss = new_loss
+        batch_gen.generate_target(prediction_probs)
 
 if args.action == "predict":
-    trainer.predict(model_dir, results_dir, features_path, vid_list_file_tst, num_epochs, actions_dict, device, sample_rate)
+    trainer.predict(model_dir, results_dir, features_path, vid_list_file_tst, num_epochs, actions_dict, device,
+                    sample_rate)
